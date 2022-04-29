93ea849700376fc2e1f4d28a9b798a4a7f5fcc58
Benjamin GALOIS <benjamin.galois@commoprices.com>
2022-04-30 01:10:12 +0200

    add img2txt

--- parse/index.js
+++ parse/index.js
@@ -2,5 +2,6 @@ const {fromXLSX} = require("./parseXLSX");
 const {fromHTML} = require("./parseHTML");
 const {fromPDF} = require("./parsePDF");
 const {fromDOCX} = require("./parseDOCX");
+const {fromIMG} = require("./parseIMG");
 
-module.exports = {fromXLSX, fromHTML, fromPDF, fromDOCX};
+module.exports = {fromXLSX, fromHTML, fromPDF, fromDOCX, fromIMG};
